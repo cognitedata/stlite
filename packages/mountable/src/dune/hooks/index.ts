ca17ebc01d6d10178edc3fe80f0557993c2e0d73
Anders Hafreager <anders.hafreager@cognite.com>
2025-09-24 08:59:04 +0200

    Fetching app file

--- packages/mountable/src/dune/hooks/index.ts
+++ packages/mountable/src/dune/hooks/index.ts
@@ -1,2 +1,4 @@
 export { useCredentials } from "./useCredentials";
 export { useIframeCredentials } from "./useIframeCommunication";
+export { useFetchFileContent } from "./useFetchFileContent";
+export type { FileContent } from "./useFetchFileContent";
