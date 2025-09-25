0c0fe5af22b2999eb93e56c364fa52b3d7192823
Lars Moastuen <lars.moastuen@cognite.com>
2025-09-25 12:48:49 +0200

    fix: add polyfills for TransformStreams, TextEncoder and TextDecoder to enable use of @zip.js/zip.js in tests

--- packages/mountable/src/setupTests.ts
+++ packages/mountable/src/setupTests.ts
@@ -1,13 +1,7 @@
-// // jest-dom adds custom jest matchers for asserting on DOM nodes.
-// // allows you to do things like:
-// // expect(element).toHaveTextContent(/react/i)
-// // learn more: https://github.com/testing-library/jest-dom
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
 // import '@testing-library/jest-dom';
 
-// Polyfill for TextEncoder/TextDecoder in Node.js test environment
-import { TextEncoder, TextDecoder } from "util";
 
-// We do as any because the Node.js TextEncoder/TextDecoder are not the same as the browser TextEncoder/TextDecoder
-// but we dont see that difference in our tests.
-global.TextEncoder = TextEncoder as any;
-global.TextDecoder = TextDecoder as any;
